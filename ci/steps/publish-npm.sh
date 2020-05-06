764d7baab9990bda72e3966e5180281f73d0c763
Anmol Sethi <hi@nhooyr.io>
2020-05-06 17:19:10 -0400

    fixup! fixup! fixup! fixup! Add NPM package, debs, rpms and refactor CI/build process

--- ci/steps/publish-npm.sh
+++ ci/steps/publish-npm.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-# We do not use the travis npm deploy integration as it does not allow us to
-# deploy a subpath and and v2 which should, just errors out that the src does not exist
 main() {
   cd "$(dirname "$0")/../.."
 
